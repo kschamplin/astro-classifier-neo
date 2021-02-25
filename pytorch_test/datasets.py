0a696c73098312f9279a5b2aa835338a53750f4f
saji <9110284+kschamplin@users.noreply.github.com>
2021-02-24 18:04:18 -0600

    add optional columns + make dataset generic (no splitting)

--- pytorch_test/datasets.py
+++ pytorch_test/datasets.py
@@ -3,8 +3,6 @@
 import torch
 import pyarrow.parquet as pq
 import numpy as np
-import pandas as pd
-
 label_map = {
     90: "SNIa",
     67: "SNIa-91bg",
@@ -29,18 +27,19 @@ label_map = {
 
 
 class plasticc_dataset(torch.utils.data.Dataset):
-    def __init__(self, file, transform=None):
+    def __init__(self, file, transform=None, cols=None):
         """Create a plasticc dataset from the given file.
         transforms argument specifies a transformer that will be called when data
         is accessed."""
         # load the file
-        cols = [
-            'true_target',
-            'mjd',
-            'flux',
-            'passband',
-            'detected_bool'
-        ]
+        if cols is None:
+            cols = [
+                'true_target',
+                'mjd',
+                'flux',
+                'passband',
+                'detected_bool'
+            ]
         self.transform = transform
         self.data = pq.read_table(file, columns=cols).to_pandas()
 
@@ -48,12 +47,7 @@ class plasticc_dataset(torch.utils.data.Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        if self.transform is not None:
-            return self.transform([
-                self.data.loc[idx].drop('true_target').values,
-                self.data.loc[idx, 'true_target']
-            ])
-        return (
-            torch.from_numpy(self.data.loc[idx].drop('true_target').values),
-            torch.tensor(self.data.loc[idx]['true_target'])
-        )
+        if callable(self.transform):
+            return self.transform(self.data.loc[idx])
+        else:
+            return self.data.loc[idx]
