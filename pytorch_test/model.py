ff59894e3560340c080f6c5745e2cf39f9613f7f
saji <saji.champlin@gmail.com>
2021-03-12 09:31:59 -0600

    add model for real

--- pytorch_test/model.py
+++ pytorch_test/model.py
@@ -1,3 +1,18 @@
 # Model generators for training/testing.
 
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DoubleLSTMNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # primary lstm layer
+        self.lstm = nn.LSTM(7, 200, 2) # 7 features, 200 hidden layers, 2 stacked.
+        self.dense = nn.Linear(200,15)
+    def forward(self, x, hidden):
+        x, hidden = self.lstm(x.float(), hidden)
+        # pad the packed sequence.
+        x = nn.utils.rnn.pad_packed_sequence(x)[0]
+        x = self.dense(x)
+        return x, hidden
