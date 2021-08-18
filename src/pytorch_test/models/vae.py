fix error in KL divergence (might still not be correct)

src/pytorch_test/models/vae.py
@@ -35,53 +35,53 @@ class VDecoder(torch.nn.Module):
         return x
 
 
+def kl_divergence(z, mu, std):
+    """MC KL Divergence"""
+
+    p = torch.distributions.Normal(torch.zeros_like(mu), torch.ones_like(std))
+    q = torch.distributions.Normal(mu, std)
+
+    log_qzx = q.log_prob(z)
+    log_pz = p.log_prob(z)
+    kl = log_qzx - log_pz
+
+    return kl.sum(-1)
+
+
 class AutoEncoder(pl.LightningModule):
     """Variational Auto-encoder model used to augment dataset and fill in space."""
 
     def __init__(self, input_channels=7, latent_dims=50):
         super().__init__()
         self.encoder = VEncoder(input_channels, 150, latent_dims)
-        self.decoder = VDecoder(latent_dims, 150, 200)
+        self.decoder = VDecoder(latent_dims, 150, input_channels)
 
         self.log_scale = nn.Parameter(torch.Tensor([0.0]))
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=1e-3)
 
-    def kl_divergence(self, z, mu, std):
-        "MC KL Divergence"
-
-        p = torch.distributions.Normal(torch.zeros_like(mu), torch.ones_like(std))
-        q = torch.distributions.Normal(mu, std)
-
-        log_qzx = q.log_prob(z)
-        log_pz = p.log_prob(z)
-        kl = log_qzx - log_pz
-
-        return kl.sum(-1)
-
     def gaussian_likelihood(self, x_hat, x):
         scale = torch.exp(self.log_scale)
         dist = torch.distributions.Normal(x_hat, scale)
 
-        print(dist.batch_shape, dist.event_shape, x.shape, x_hat.shape)
         log_pzx = dist.log_prob(x)
 
-        return log_pzx.sum(dim=(1,2,3))
+        return log_pzx.sum(dim=(1, 2))
 
     def training_step(self, batch, batch_idx):
         x, _ = batch  # discard true class (don't care)
 
         x_mean, x_std = self.encoder(x)  # get probs in latent space for x
 
-        x_std = torch.exp(x_std / 2) # log std dev
+        x_std = torch.exp(x_std / 2)  # log std dev
         q = torch.distributions.Normal(x_mean, x_std)
-        z = q.rsample() # sample the distribution
+        z = q.rsample()  # sample the distribution
 
         x_hat = self.decoder(z)
 
         recon_loss = self.gaussian_likelihood(x_hat, x)
-        kl_loss = self.kl_divergence(z, x_mean, x_std)
+        kl_loss = kl_divergence(z, x_mean, x_std)
 
         elbo = kl_loss - recon_loss
         elbo = elbo.mean()