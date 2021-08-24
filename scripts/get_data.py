55468f8d80c21237c31a75822c302c7302d9d146
saji <9110284+kschamplin@users.noreply.github.com>
2021-08-24 14:43:14 -0500

    update get data script

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -1,55 +1,6 @@
 #!/usr/bin/env python3
 # downloads plasticc data and optionally preprocesses it.
-import gzip
-import hashlib
-import shutil
-from pathlib import Path
 
-import requests
-from tqdm import tqdm
+from pytorch_test.plasticc import PlasticcDataModule
 
-blacklist = [
-    "plasticc_modelpar.tar.gz"
-]
-data_dir = Path("./data")
-
-
-def download():
-    # create data dir
-    if not data_dir.exists():
-        data_dir.mkdir()
-    # scrape the file list
-    print("Gathering file list")
-    files = requests.get(
-        "https://zenodo.org/api/records/2539456").json()['files']
-    files = [f for f in files if f['key'] not in blacklist]
-
-    for f in tqdm(files):
-        # TODO: add checksum validation
-        r = requests.get(f['links']['self'], stream=True)
-        size = int(r.headers.get('content-length', 0))
-        bs = 1024
-        pbar = tqdm(total=size, unit="iB", unit_scale=True)
-        pbar.set_description(f"{f['key']}")
-        with (data_dir / f['key']).open(mode='wb') as dest:
-            md5 = hashlib.md5()
-            for data in r.iter_content(bs):
-                dest.write(data)
-                pbar.update(len(data))
-                md5.update(data)
-            print(md5.hexdigest())
-        pbar.close()
-
-
-def gunzip(data_dir):
-    # unzips all the files
-    # get list of all gz files
-    gzs = data_dir.glob('*.gz')
-    for gzpath in tqdm(gzs):
-        with gzip.open(gzpath, 'rb') as input_file:
-            with open(gzpath.parent / gzpath.stem, 'wb') as output:
-                shutil.copyfileobj(input_file, output)
-
-
-if __name__ == "__main__":
-    gunzip()
+PlasticcDataModule(download=True).prepare_data()
